6f0e8340dda11fb894aa59955e9320356f314aff
harsh12uv <harsharaj2099@gmail.com>
2024-02-24 15:55:25 +0530

    try 3

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 
             public void addMeeting(Meeting meeting){
                 //add the meeting to calendar
+
                 calendar.add(meeting);
 
             }
@@ -28,9 +29,9 @@ import java.util.Collections;
                 // 1. At a particular time, you can be present in at most one meeting
                 // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
                 // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-                Collections.sort(calendar, (m1, m2) -> m1.getEnd().compareTo(m2.getEnd()));
+                Collections.sort(calendar, (m1, m2) -> m1.getStart().compareTo(m2.getStart()));
 
-                int maxMeetings = 0;
+                int maxMeetings = 1;
                 LocalTime lastEndTime =calendar.get(0).getEnd(); // Initialize with the smallest possible value
 
                 // Iterate through sorted meetings
